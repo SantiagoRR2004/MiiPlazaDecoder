70707332b959256957677183e291fc4c84dd524a
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-07-19 19:53:49 +0200

    Add MiiPlaza

---
+++ miiPlaza.py
@@ -0,0 +1,38 @@
+import mii
+
+class MiiPlaza:
+    MII_PLAZA_SIZE = 393216
+    
+    def __init__(self, bytesData: bytes) -> None:
+        """
+        Initialize MiiPlaza object with bytes data
+        
+        Args:
+            - bytesData (bytes): The raw bytes data of the Mii
+
+        Returns:
+            - None
+        """
+        assert len(bytesData) == self.MII_PLAZA_SIZE, "Invalid Mii Plaza size"
+        self.bytesData = bytesData
+
+    def setMiis(self) -> None:
+        """
+        Set all Mii attributes by decoding the bytes data
+        They are stored beginning on the 14154 byte.
+
+        Args:
+            - None
+            
+        Returns:
+            - None
+        """
+        miis = []
+        pos = 14154
+
+        while self.bytesData[pos] != 0:
+            miiData = self.bytesData[pos:pos + mii.Mii.MII_SIZE]
+            miis.append(mii.Mii(miiData))
+            pos += mii.Mii.MII_SIZE
+
+        self.miis = miis
