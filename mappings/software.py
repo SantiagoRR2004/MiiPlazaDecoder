4594e016c95a7fc9e496105ccd5774aa6935a59e
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2025-07-22 20:37:40 +0200

    Enhance title retrieval from hshop

--- mappings/software.py
+++ mappings/software.py
@@ -5,15 +5,93 @@ import os
 
 
 def titleFromhshop(gameID: str) -> str:
+    """
+    Get the game name from hshop based on the game ID.
+
+    It will try to find the english name of the game
+    by searching for the product code.
+
+    Args:
+        - gameID (str): The ID of the game to search for.
+
+    Returns:
+        - str: The name of the game if found, otherwise "Unknown Game".
+    """
     url = f"https://hshop.erista.me/search/results?q={gameID}&qt=TitleID"
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, "html.parser")
-        base_info = soup.find("div", class_="base-info")
-        if base_info:
-            game_name_tag = base_info.find("h3", class_="green bold nospace")
-            if game_name_tag:
-                return game_name_tag.text.strip()
+
+        name = None
+        productCode = None
+        region = None
+        priority = {"north-america": 1, "europe": 2, "world": 3}
+
+        # Get the first result entry
+        entry = soup.find("a", class_="list-entry block-link")
+
+        if entry:
+            # Find all meta-content blocks
+            meta_contents = entry.find_all("div", class_="meta-content")
+            for meta in meta_contents:
+                spans = meta.find_all("span")
+                if len(spans) >= 2:
+                    label = spans[1].text.strip()
+                    value = spans[0].text.strip()
+                    if label == "Product Code":
+                        productCode = value
+                    elif label == "Title ID":
+                        assert value == gameID, "Something is wrong with hShop"
+
+            # Get game name
+            base_info = entry.find("div", class_="base-info")
+            if base_info:
+                game_name_tag = base_info.find("h3", class_="green bold nospace")
+                if game_name_tag:
+                    name = game_name_tag.text.strip()
+
+            # Get the region
+            h4_tags = base_info.find_all("h4")
+            for h4_tag in h4_tags:
+                spans = h4_tag.find_all("span", class_="green bold")
+                if len(spans) >= 2:
+                    region = spans[1].text.strip()
+
+        # Try to find name in English
+        if productCode:
+            # Remove last character to get all regions
+            url = f"https://hshop.erista.me/search/results?q={productCode[:-1]}&qt=ProductCode"
+
+            response = requests.get(url)
+            if response.status_code == 200:
+                soup = BeautifulSoup(response.text, "html.parser")
+
+                # Get the all the entries
+                entries = soup.find_all("a", class_="list-entry block-link")
+
+                for entry in entries:
+
+                    base_info = entry.find("div", class_="base-info")
+
+                    # Get current region
+                    if base_info:
+                        foundRegion = None
+
+                        h4_tags = base_info.find_all("h4")
+                        for h4_tag in h4_tags:
+                            spans = h4_tag.find_all("span", class_="green bold")
+                            if len(spans) >= 2:
+                                foundRegion = spans[1].text.strip()
+
+                        if priority.get(foundRegion, 0) > priority.get(region, 0):
+                            region = foundRegion
+                            name = base_info.find(
+                                "h3", class_="green bold nospace"
+                            ).text.strip()
+
+        if name:
+            return name
+
     return "Unknown Game"
 
 
